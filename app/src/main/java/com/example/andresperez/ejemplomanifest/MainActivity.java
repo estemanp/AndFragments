2db8865e80c3cf1f42d0551a70ad0ddf3eea47e7
estemanp <estemanp@MacBook-Pro-de-Mac-estemanp.local>
2016-10-03 16:55:04 -0500

    Commit: se implementa la funcionalidad de cambiar un fragment

--- app/src/main/java/com/example/andresperez/ejemplomanifest/MainActivity.java
+++ app/src/main/java/com/example/andresperez/ejemplomanifest/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.andresperez.ejemplomanifest;
 
+
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements EjemploFragment.OnFragmentInteractionListener {
+import static com.example.andresperez.ejemplomanifest.OtroFragment.*;
+
+public class MainActivity extends AppCompatActivity implements EjemploFragment.OnFragmentInteractionListener, OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,42 @@ public class MainActivity extends AppCompatActivity implements EjemploFragment.O
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    public void changeFragment(View view) {
+        //Paso 1: Obtener la instancia del administrador de fragmentos
+        FragmentManager fragmentManager = getFragmentManager();
+
+        //Paso 2: Crear una nueva transacción
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        if(currentFragmentIsEjemploFragment()){
+            //Paso 3: Crear un nuevo fragmento y reemplazarlo
+            OtroFragment fragment = new OtroFragment();
+            transaction.replace(R.id.containerFragment, fragment);
+            transaction.addToBackStack("other_fragment");
+        }else{
+            //Paso 3: Crear un nuevo fragmento y reemplazarlo
+            EjemploFragment fragment = new EjemploFragment();
+            transaction.replace(R.id.containerFragment, fragment);
+            transaction.addToBackStack("ejemplo_fragment");
+        }
+
+        //Paso 4: Confirmar el cambio
+        transaction.commit();
+    }
+
+    private  boolean currentFragmentIsEjemploFragment(){
+        Fragment currentFragment = getFragmentManager().findFragmentById(R.id.containerFragment);
+        if(currentFragment != null){
+            if (!currentFragment.getClass().getName().equalsIgnoreCase(EjemploFragment.class.getName())) {
+                //El actual fragment no coincide con EjemploFragment
+                return false;
+            } else {
+                //El actual fragment  coincide con EjemploFragment
+                return true;
+            }
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/andresperez/ejemplomanifest/OtroFragment.java
+++ app/src/main/java/com/example/andresperez/ejemplomanifest/OtroFragment.java
@@ -12,14 +12,15 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link EjemploFragment.OnFragmentInteractionListener} interface
+ * {@link OtroFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  */
-public class EjemploFragment extends Fragment {
+public class OtroFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public EjemploFragment() {
+    public OtroFragment() {
+        // Required empty public constructor
     }
 
 
@@ -27,7 +28,7 @@ public class EjemploFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ejemplo, container, false);
+        return inflater.inflate(R.layout.fragment_otro, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
