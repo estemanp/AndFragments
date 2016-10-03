7e902a19b466cb572fb9d012804685076074f0b4
Andres Esteban Perez <andres.perez@ceiba.com.co>
2016-10-03 11:50:59 -0500

    Commit: Se agrega pry Android

---
+++ app/src/main/java/com/example/andresperez/ejemplomanifest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.andresperez.ejemplomanifest;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements EjemploFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
